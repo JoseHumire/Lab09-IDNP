4bf1245bc599b0252b717d40f9395bcfb8508821
CesarSamanez <csamanezj@unsa.edu.pe>
2021-11-24 23:05:14 -0500

    Sending data

--- app/src/main/java/com/example/lab09_idnp/Fragments/CreateServiceFragment.java
+++ app/src/main/java/com/example/lab09_idnp/Fragments/CreateServiceFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
@@ -11,18 +14,33 @@ import com.example.lab09_idnp.R;
 
 
 public class CreateServiceFragment extends Fragment {
+    private EditText inputSchedule, inputSalary, inputProfession;
+    private Button saveBtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-    public View onCreateView(
-            @NonNull LayoutInflater inflater,
-            ViewGroup container,
-            Bundle savedInstanceState
-    )
-    {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_create_service, container, false);
+
+        inputSalary = view.findViewById(R.id.textInputEditTextSalary);
+        inputSchedule = view.findViewById(R.id.textInputEditTextSchedule);
+        inputProfession = view.findViewById(R.id.textInputEditTextProfession);
+        saveBtn = view.findViewById(R.id.btnSave);
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle result = new Bundle();
+                result.putString("Salary", inputSalary.getText().toString());
+                result.putString("Schedule", inputSchedule.getText().toString());
+                result.putString("Profession", inputProfession.getText().toString());
+
+            }
+        });
+
         return view;
     }
 }
